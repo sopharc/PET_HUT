404996783fbbfa00a4c76ac9c038db7470cf4a14
Sophie Archer <hello@sophiearcher.co.uk>
2020-06-30 20:18:04 +0100

    added some crud actions to customer and order controllers

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -3,7 +3,21 @@ class CustomerController < ApplicationController
     @customer = Customer.new
   end
 
-  def edit
+  def create
+    @customer = Customer.new(customer_params)
+    @customer.save
 
+    redirect_to home_path
   end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:user_id, :phone_number, :first_name, :last_name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'order/new'
+  get 'order/create'
+  get 'order/edit'
+  get 'order/update'
+  get 'order/destroy'
   get 'user/new'
   get 'user/create'
   get 'user/edit'
@@ -7,6 +12,6 @@ Rails.application.routes.draw do
   get 'customer/new', to: 'customer#new'
   get 'customer/edit', to: 'customer#edit'
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#home', as: :home
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
