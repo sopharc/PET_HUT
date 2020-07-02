ec9bc63d0d3414fa5b25f1f7d229cdd877550865
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-07-02 19:30:32 +0100

    Added a components sandbox html and button partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'customer/new', to: 'customer#new'
   get 'customer/edit', to: 'customer#edit'
+  get 'components', to: 'pages#components'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
