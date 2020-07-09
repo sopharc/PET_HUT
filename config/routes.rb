0748a29e1f1e7f332e1c20c79d01fc60c24d2d26
Sophie Archer <hello@sophiearcher.co.uk>
2020-07-09 19:51:58 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,31 +2,16 @@ Rails.application.routes.draw do
   root to: 'pages#home', as: :home
   devise_for :users
 
-  resources :customers do
-    resources :orders do
-      resources :order_products
+  resources :customers
+
+  resources :orders
 
   resources :addresses
 
-resources :components do
-  resources :products do
-    resources :product_materials, only: [:new, :create]
+  resources :components
 
+  resources :products
 
-  get 'order/new'
-  get 'order/create'
-  get 'order/edit'
-  get 'order/update'
-  get 'order/destroy'
-  get 'user/new'
-  get 'user/create'
-  get 'user/edit'
-  get 'user/update'
-  get 'user/destroy'
-  get 'customer/new', to: 'customer#new'
-  get 'customer/edit', to: 'customer#edit'
   get 'components', to: 'pages#components'
 
-  end
- end
 end
