88a11063aec8810f644200e9c84b62a4e0cef015
David Egerton <david.egertonsmith@gmail.com>
2020-07-09 20:33:29 +0100

    index page with action and links to show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
-<<<<<<< HEAD
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
+
   def index
     @products = Product.all
-=======
-  skip_before_action :authenticate_user!, only: [ :show ]
+  end
+
   def show
     @product = Product.find(params[:id])
->>>>>>> master
   end
 end
