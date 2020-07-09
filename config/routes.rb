775666d50181589bc95b0d45d50d8af50399251d
Vladi Angelov <vladislav_angelov@yahoo.com>
2020-07-09 20:09:19 +0100

    Added products show route, controller action and view / excluded from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
-  get 'order/new'
-  get 'order/create'
-  get 'order/edit'
-  get 'order/update'
-  get 'order/destroy'
-  get 'user/new'
-  get 'user/create'
-  get 'user/edit'
-  get 'user/update'
-  get 'user/destroy'
-  get 'customer/new', to: 'customer#new'
-  get 'customer/edit', to: 'customer#edit'
+  resources :products, only: [:index, :show]
+
   get 'components', to: 'pages#components'
   devise_for :users
   root to: 'pages#home', as: :home
