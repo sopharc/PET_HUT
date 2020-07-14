465c95afeef3274347d53c6c9552ad048ec83d29
Yasmin Jones-Sheahan <y.jsheahan@gmail.com>
2020-07-14 21:16:51 +0100

    added validation for email

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ApplicationRecord
   has_many :orders
 
+  belongs_to :user
   validates :first_name, presence: true, uniqueness: true
   validates :last_name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one :customer
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
+  validates :email, presence: true
 
   # for Active Admin
   def name
