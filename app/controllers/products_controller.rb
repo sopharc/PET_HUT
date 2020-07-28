4df3da3c642756388604e0d60b683c2f1f42ebcf
David Egerton <david.egertonsmith@gmail.com>
2020-07-28 20:13:30 +0100

    adding latest filter attempts for help

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,19 @@ class ProductsController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
 
   def index
-    @products = Product.all
-    @products = Product.filter_by_category(params[:category]) if params[:category].present?
+    if params[:query].present?
+      @products = Product.where(category: params[:query])
+    else
+      @products = Product.all
+    end
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  private
+  def search_params
+    params.require(:product).permit(:query)
   end
 end
