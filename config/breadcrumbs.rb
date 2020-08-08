0e734e8af959891bd69e05928058219c3c28615e
Sophie Archer <hello@sophiearcher.co.uk>
2020-08-08 19:52:48 +0100

    breadcrumbs now working for home/product/show with links

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -12,23 +12,6 @@ crumb :product do |product|
 end
 
 
-
-
-# crumb :project do |project|
-#   link project.name, project_path(project)
-#   parent :projects
-# end
-
-# crumb :project_issues do |project|
-#   link "Issues", project_issues_path(project)
-#   parent :project, project
-# end
-
-# crumb :issue do |issue|
-#   link issue.title, issue_path(issue)
-#   parent :project_issues, issue.project
-# end
-
 # If you want to split your breadcrumbs configuration over multiple files, you
 # can create a folder named `config/breadcrumbs` and put your configuration
 # files there. All *.rb files (e.g. `frontend.rb` or `products.rb`) in that
