f58318981616557d4ecfd9a61a96db8909be0b6c
Sophie Archer <hello@sophiearcher.co.uk>
2020-08-18 20:51:56 +0100

    search form added to index but not yet searching

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,13 @@ class Product < ApplicationRecord
   # scope for product index to allow filtering
   scope :filter_by_category, -> (category) { where category: category }
 
+  include PgSearch::Model
+  pg_search_scope :search_by_name,
+    against: [ :name, :syllabus ],
+    using: {
+      tsearch: { prefix: true }
+    }
+
   private
 
   def not_referenced_by_any_line_item
