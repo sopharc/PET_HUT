aeed47b1999bad41ca3034d9b29a3184d7bc93bd
David Egerton <david.egertonsmith@gmail.com>
2020-10-07 10:24:34 +0100

    fixed front page text in addition to basic layout for checkout summary

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,4 +1,5 @@
 class PaymentsController < ApplicationController
+  include CurrentCart
   def new
     @order = current_user.customer.orders.where(status: 'pending').find(params[:order_id])
     authorize @order
