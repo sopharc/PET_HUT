4664e9444861a2e80a2a6df6371564df836b0bc0
Sophie Archer <hello@sophiearcher.co.uk>
2020-10-12 19:45:49 +0100

    breadcrumbs removed

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+
+
+  def index
+    @contact = Contact.new(params[:contact])
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    respond_to do |format|
+      if @contact.deliver
+        # re-initialize Home object for cleared form
+        @contact = Contact.new
+        format.html { render 'index'}
+        format.js   { flash.now[:success] = @message = "Thank you for your message. We will get back to you soon!" }
+      else
+        format.html { render 'index' }
+        format.js   { flash.now[:error] = @message = "Message did not send." }
+      end
+    end
+  end
+end
