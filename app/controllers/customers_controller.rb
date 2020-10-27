1c446ba78129567878e9789e52dab3724ed43a22
Sophie Archer <hello@sophiearcher.co.uk>
2020-10-27 20:52:31 +0000

    customer set up working but mail not sending

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -19,9 +19,8 @@ skip_before_action :authenticate_user!, only: [:new]
     @customer.user_id = current_user.id
 
     authorize @customer
-
     if @customer.save
-       mail = UserMailer.with(user: @user).welcome
+       mail = UserMailer.with(user: current_user).welcome
        mail.deliver_now
       redirect_to products_path, notice: 'Account successfully created!'
     else
